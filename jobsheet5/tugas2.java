a36970c2350cc333aa5b76779d5caca8e9507a89
evan1234-map <evanfernanda1708@gmail.com>
2025-10-10 13:59:33 +0700

    Tugas 2

--- jobsheet5/tugas2.java
+++ jobsheet5/tugas2.java
@@ -6,14 +6,14 @@ public class tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int angka = sc.nextInt();
         System.out.print("Masukan angka:");
-        
+        int angka = sc.nextInt();
+             
         if (angka % 2 == 0){
             System.out.println("Angka Genap");
         } else {
             System.out.println("Angka Ganjil");
-            sc.close();
+
         }
     }
 }
