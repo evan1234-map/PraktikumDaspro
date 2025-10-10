02a1f8414821cbf71548ed58696da255acf394df
evan1234-map <evanfernanda1708@gmail.com>
2025-10-10 13:48:20 +0700

    percobaan 7

---
+++ jobsheet5/tugas2.java
@@ -0,0 +1,19 @@
+//package jobsheet5;
+
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int angka = sc.nextInt();
+        System.out.println("Masukan angka:");
+        
+        if (angka % 2 == 0){
+            System.out.println("Angka Genap");
+        } else {
+            System.out.println("Angka Ganjil");
+            sc.close();
+        }
+    }
+}
